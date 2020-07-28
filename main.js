b17b0c536aef84b443f1ee5fbc775d0db9334c18
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-07-28 12:20:54 -0500

    a.byrd helped improve image error handling

--- main.js
+++ main.js
@@ -14,9 +14,15 @@ function displayResults(responseJson) {
   console.log(responseJson);
   $('.js-results, #js-error-message').empty();
   for (let i = 0; i < responseJson.data.length; i++){
+    if (!responseJson.data[i]){
+      continue;
+    }
     $('.js-results').append(`
       <h2>${responseJson.data[i].fullName}</h2>
       <p>${responseJson.data[i].description}</p>
+      ${responseJson.data[i].images && responseJson.data[i].images[0] ?
+         `<img class="photo" src="${responseJson.data[i].images[0].url}" alt="${responseJson?.data[i].images[0].altText}">` :
+          ``}
       <p class="centered more">Learn more at <br>
       <a href="${responseJson.data[i].url}" target="bank">${responseJson.data[i].url}</a></p>`
     )};
